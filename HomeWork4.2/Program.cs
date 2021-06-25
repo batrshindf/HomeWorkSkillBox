008866924dc1d7557c24f0aa895e33928db5ff82
Денис Батршин <batrshindf@yandex.ru>
2021-06-25 23:09:25 +0300

    Создан метод вычисления факториала.

--- HomeWork4.2/Program.cs
+++ HomeWork4.2/Program.cs
@@ -8,8 +8,34 @@ namespace HomeWork4._2
 {
     class Program
     {
+        /// <summary>
+        /// Вычисление факториала
+        /// </summary>
+        /// <param name="number">Числло фаториала которое надо вычислить</param>
+        /// <returns>Результат факториала</returns>
+        static float GetFactorial(int number)
+        {
+            float x = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                x *= i;
+            }
+
+            return x;
+        }
+
+        /// <summary>
+        /// Основной метод
+        /// </summary>
         static void Main(string[] args)
         {
+            Console.Write("Введите размер треугольника (количество строк в треугольнике): ");
+            int triangleSize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+          
+
+            Console.ReadKey();
         }
     }
 }
