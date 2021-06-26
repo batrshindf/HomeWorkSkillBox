05c5d85d05bddb7fd111400277d467f9d5e2d745
Денис Батршин <batrshindf@yandex.ru>
2021-06-26 05:22:57 +0300

    Готовый треугольник

--- HomeWork4.2/Program.cs
+++ HomeWork4.2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork4._2
 {
@@ -23,17 +19,30 @@ namespace HomeWork4._2
 
             return x;
         }
-
+        
         /// <summary>
         /// Основной метод
         /// </summary>
         static void Main(string[] args)
         {
-            Console.Write("Введите размер треугольника (количество строк в треугольнике): ");
+            Console.Write("Введите размер треугольника (количество строк в треугольнике), не более 25 строк: ");
             int triangleSize = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-          
+            for (int i = 0; i < triangleSize; i++)
+            {
+                for (int j = 0; j <= (triangleSize - i); j++) // Создание отступа
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($" {GetFactorial(i) / (GetFactorial(j) * GetFactorial(i - j))}");
+                }
+
+                Console.WriteLine();
+            }
 
             Console.ReadKey();
         }
