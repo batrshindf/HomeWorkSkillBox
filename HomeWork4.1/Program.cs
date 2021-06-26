9f55f123f425a5f7836213881979c5812b341d18
Денис Батршин <batrshindf@yandex.ru>
2021-06-26 22:54:21 +0300

    Исправлена ошибка, возникавшая,, когда вся прибыль одинакова.

--- HomeWork4.1/Program.cs
+++ HomeWork4.1/Program.cs
@@ -2,100 +2,106 @@
 
 namespace HomeWork4._1
 {
-    class Program
+    internal class Program
     {
         /// <summary>
-        /// Заполение массива
+        ///     Заполение массива
         /// </summary>
         /// <param name="array">Массив, который нужно заполнить</param>
         /// <param name="random">Массив псевдо-случайных чисел</param>
         /// <returns>Массив, заполненный случайными числами</returns>
-        static int[] GetFilledArray(int[] array, Random random)
+        private static int[] GetFilledArray(int[] array, Random random)
         {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(1000);
-            }
+            for (int i = 0; i < array.Length; i++) array[i] = random.Next(1000);
 
             return array;
         }
 
         /// <summary>
-        /// Вывод таблицы финансов компании за 1 год
+        ///     Вывод таблицы финансов компании за 1 год
         /// </summary>
         /// <param name="income">Доход</param>
         /// <param name="expenses">Расходы</param>
         /// <param name="profit">Прибыль</param>
-        static void OutputOfTheFinancialTable(int[] income, int[] expenses, int[] profit)
+        private static void OutputOfTheFinancialTable(int[] income, int[] expenses, int[] profit)
         {
             //Вывод шапки тыблицы
             Console.WriteLine("|\tМесяц\t|\tДоходы\t\t|\tРасходы\t\t|\tПрибыль\t\t|");
-            
+
             //Вывод значений таблицы
             for (int i = 0; i < profit.Length; i++)
-            {
                 Console.WriteLine($"|\t{i + 1}\t|\t{income[i]}\t\t|\t{expenses[i]}\t\t|\t{profit[i]}\t\t|");
-            }
         }
 
         /// <summary>
-        /// Вывод наихужших месяцев по прибыли.
+        ///     Вывод наихужших месяцев по прибыли.
         /// </summary>
         /// <param name="profit">Прибыль</param>
-        static void BadMonths(int[] profit)
+        private static void BadMonths(int[] profit)
         {
             int[] copyProfit = new int[12];
-            int c = 3;  //3 наихудших месяца, без одинаковых месяцев
+            int c = 3; //3 наихудших месяца, без одинаковых месяцев
+            byte count = 0;
 
-            for (int i = 0; i < copyProfit.Length; i++)
-            {
-                copyProfit[i] = profit[i];
-            }
+            for (int i = 0; i < copyProfit.Length; i++) copyProfit[i] = profit[i];
 
             Array.Sort(copyProfit);
 
             Console.Write("\nНаихудшие месяцы: ");
-            for (int i = 0; i < c; i++)
-            {
-                if (copyProfit[i] == copyProfit[i + 1])
+
+            for (int i = 0; i < profit.Length; i++)
+                if (copyProfit[0] == profit[i])
                 {
-                    c++;
-                    continue;
+                    Console.Write($" {i + 1}");
+                    count++;
                 }
 
-                for (int j = 0; j < profit.Length; j++)
+            if (count != profit.Length)
+            {
+                count = 0;
+
+
+                for (int i = 1; i < c; i++)
                 {
-                    if (copyProfit[i] == profit[j])
+                    if (copyProfit[i] == copyProfit[i - 1])
                     {
-                        Console.Write($" {j + 1}");
+                        c++;
+                        if (c == copyProfit.Length) break;
+
+                        continue;
                     }
+
+                    for (int j = 0; j < profit.Length; j++)
+                        if (copyProfit[i] == profit[j])
+                        {
+                            Console.Write($" {j + 1}");
+
+                            count++;
+                            if (count == profit.Length) break;
+                        }
                 }
             }
         }
 
         /// <summary>
-        /// Подсчет и вывод на экран консоли кол-во месяцев с положительной прибылью
+        ///     Подсчет и вывод на экран консоли кол-во месяцев с положительной прибылью
         /// </summary>
         /// <param name="profit">Прибыль</param>
-        static void PositiveProfit(int[] profit)
+        private static void PositiveProfit(int[] profit)
         {
             byte count = 0;
 
             for (int i = 0; i < profit.Length; i++)
-            {
                 if (profit[i] > 0)
-                {
                     count++;
-                }
-            }
 
             Console.WriteLine($"\nКоличество месяцев с положительной прибылью равно {count}.");
         }
 
         /// <summary>
-        /// Основной метод
+        ///     Основной метод
         /// </summary>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             #region Переменные
 
@@ -105,17 +111,14 @@ namespace HomeWork4._1
             int[] profit = new int[12];
 
             #endregion
-            
+
             //Заполнение массивов Прибыли и Расходов
             GetFilledArray(income, random);
             GetFilledArray(expenses, random);
 
             //Находим прибыль
-            for (int i = 0; i < profit.Length; i++)
-            {
-                profit[i] = income[i] - expenses[i];
-            }
-            
+            for (int i = 0; i < profit.Length; i++) profit[i] = income[i] - expenses[i];
+             
             OutputOfTheFinancialTable(income, expenses, profit);
             BadMonths(profit);
             PositiveProfit(profit);
