ad37ccc3b7bf562e0ae60f84bf00aadde7d9cb23
Денис Батршин <batrshindf@yandex.ru>
2021-06-27 00:48:27 +0300

    Выполнена работа по треугольника Паскаля. Приведено к единому Стилю с помощью Решарда

--- HomeWork4.2/Program.cs
+++ HomeWork4.2/Program.cs
@@ -2,47 +2,116 @@
 
 namespace HomeWork4._2
 {
-    class Program
+    internal class Program
     {
+        private const int cellWidth = 4;
+
         /// <summary>
-        /// Вычисление факториала
+        ///     Вывод треугольника Паскаля в красивом виде
         /// </summary>
-        /// <param name="number">Числло фаториала которое надо вычислить</param>
-        /// <returns>Результат факториала</returns>
-        static float GetFactorial(int number)
+        /// <param name="row">Количество строк в треугольнике</param>
+        /// <param name="trianglePascal">Заполненный треугольник Паскаля</param>
+        private static void OutputTriangle2(int row, int[,] trianglePascal)
         {
-            float x = 1;
-            for (int i = 1; i <= number; i++)
+            int col = cellWidth * row;
+
+            for (int i = 0; i < row; i++)
             {
-                x *= i;
-            }
-
-            return x;
-        }
-        
-        /// <summary>
-        /// Основной метод
-        /// </summary>
-        static void Main(string[] args)
-        {
-            Console.Write("Введите размер треугольника (количество строк в треугольнике), не более 25 строк: ");
-            int triangleSize = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            for (int i = 0; i < triangleSize; i++)
-            {
-                for (int j = 0; j <= (triangleSize - i); j++) // Создание отступа
-                {
-                    Console.Write(" ");
-                }
-
                 for (int j = 0; j <= i; j++)
                 {
-                    Console.Write($" {GetFactorial(i) / (GetFactorial(j) * GetFactorial(i - j))}");
+                    Console.SetCursorPosition(col, i + 4);
+                    if (trianglePascal[i, j] != 0)
+                    {
+                        Console.Write($"{trianglePascal[i, j],cellWidth}");
+                    }
+
+                    col += cellWidth * 2;
+                }
+
+                col = cellWidth * row - cellWidth * (i + 1);
+
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        ///     Заполнение треугольника Паскаля
+        /// </summary>
+        /// <param name="row">Количество строк в треугольнике</param>
+        /// <param name="trianglePascal">Заполненный треугольник Паскаля</param>
+        private static void FillInTheTriangle(int row, int[,] trianglePascal)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                trianglePascal[i, 0] = 1;
+                trianglePascal[i, i] = 1;
+            }
+
+            for (int i = 2; i < row; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    trianglePascal[i, j] = trianglePascal[i - 1, j - 1] + trianglePascal[i - 1, j];
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Вывод треугольника Паскаля в простом виде
+        /// </summary>
+        /// <param name="row">Количество строк в треугольнике</param>
+        /// <param name="trianglePascal">
+        ///     Заполненный треугольник Паскаля<</param>
+        private static void OutputTriangle(int row, int[,] trianglePascal)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    if (trianglePascal[i, j] != 0)
+                    {
+                        Console.Write($"{trianglePascal[i, j],cellWidth}");
+                    }
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        /// <summary>
+        ///     Проверка на правильность ввода количества строк треугольника.
+        /// </summary>
+        /// <param name="row">Количество строк треугольника</param>
+        /// <returns>Количество строк треугольника не более 25.</returns>
+        private static int CheckInputRow(int row)
+        {
+            while (row > 25)
+            {
+                Console.Write("\nУкажите правильно количество строк в треугольнике (не более 25): ");
+                row = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            return row;
+        }
+
+        /// <summary>
+        ///     Основной метод
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите размер треугольника (количество строк в треугольнике), не более 25 строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            row = CheckInputRow(row);
+            int[,] trianglePascal = new int[row, row];
+
+            FillInTheTriangle(row, trianglePascal);
+
+            //OutputTriangle(row, trianglePascal);  //Для вывода в простом виде надо изменить значение cellWidth с 4 на 8 и закоментировать функцию OutputTriangle2.
+
+            OutputTriangle2(row, trianglePascal); //Для вывода в красивом виде надо изменить значение cellWidth с 8 на 4 и закоментировать функцию OutputTriangle.
 
             Console.ReadKey();
         }
