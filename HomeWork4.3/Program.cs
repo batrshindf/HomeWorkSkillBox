259ee25e5863db3792eb446798bf8fcbfb46fddd
Денис Батршин <batrshindf@yandex.ru>
2021-06-27 07:29:34 +0300

    Добавление проекта По Матрицам

---
+++ HomeWork4.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4._3
+{
+    class Program
+    {
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите высоту матрицы: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.Write("\nВведите ширину матрицы: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+
+            FillMatrix(row, col, matrix);
+        }
+
+        private static void FillMatrix(int row, int col, int[,] matrix)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                }
+            }
+        }
+    }
+}
