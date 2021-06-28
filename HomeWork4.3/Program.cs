82a5110c1b9d82ac54a41dfb0e2dc5fa5068c84b
Денис Батршин <batrshindf@yandex.ru>
2021-06-28 22:00:12 +0300

    Добавление метода проверки ввода параметров массива с клавиатуры

--- HomeWork4.3/Program.cs
+++ HomeWork4.3/Program.cs
@@ -17,15 +17,43 @@ namespace HomeWork4._3
             
             Console.Write("Введите высоту матрицы: ");
             int row = int.Parse(Console.ReadLine());
+            CheckInput(row);
 
             Console.Write("\nВведите ширину матрицы: ");
             int col = int.Parse(Console.ReadLine());
+            CheckInput(col);
+
+
 
             int[,] matrix = new int[row, col];
 
             FillMatrix(row, col, matrix);
+
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// Проверка ввода на отрицательные и нулевые данные для размерности массива
+        /// </summary>
+        /// <param name="number">Воод параметра количесва строк или ширрины</param>
+        /// <returns>Правильная мерность массива</returns>
+        private static int CheckInput(int number)
+        {
+            while (number <= 0)
+            {
+                Console.Write("Введите правильный параметр матрицы (долженбыть положительный и не равен 0): ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Заполнеине матрицы
+        /// </summary>
+        /// <param name="row">Количество строк матрицы</param>
+        /// <param name="col">Количество колонок матрицы</param>
+        /// <param name="matrix">Матрица, ввиде двумерного массива</param>
         private static void FillMatrix(int row, int col, int[,] matrix)
         {
             Random random = new Random();
