23eb137b8cf6ba8b4768392daea178709520837e
Денис Батршин <batrshindf@yandex.ru>
2021-07-22 00:56:27 +0300

    Доработан метод перемножения матриц. Исправлена ошибка, когда значения исхоной матрицы менялось

--- HomeWork5.1/Program.cs
+++ HomeWork5.1/Program.cs
@@ -73,7 +73,7 @@ namespace HomeWork5._1
             {
                 for (int j = 0; j < GetMatrixCol(matrix); j++)
                 {
-                    matrix[i, j] = random.Next(-100, 101);
+                    matrix[i, j] = random.Next(-10, 11);
                 }
             }
         }
@@ -125,6 +125,8 @@ namespace HomeWork5._1
             PrintMatrix(matrixB);
         }
 
+        #region Методы вычесления матриц
+
         /// <summary>
         /// Умножение матрицы на число
         /// </summary>
@@ -134,18 +136,18 @@ namespace HomeWork5._1
             Console.Clear();
             Console.WriteLine("\n\n Умножение матрицы на число!");
             Console.Write("\nВведите число на которое нужно умножить матрицу: ");
-            
+
             var multipliByNumber = int.Parse(Console.ReadLine());
-            var matrixC = matrix;
+            var matrixC = new int[GetMatrixRow(matrix), GetMatrixCol(matrix)];
 
             Console.WriteLine("\nИсходная матрица:");
             PrintMatrix(matrix);
 
-            for (int i = 0; i < GetMatrixRow(matrixC); i++)
+            for (int i = 0; i < GetMatrixRow(matrix); i++)
             {
-                for (int j = 0; j < GetMatrixCol(matrixC); j++)
+                for (int j = 0; j < GetMatrixCol(matrix); j++)
                 {
-                    matrixC[i, j] *= multipliByNumber;
+                    matrixC[i, j] = matrix[i, j] * multipliByNumber;
                 }
             }
 
@@ -162,25 +164,24 @@ namespace HomeWork5._1
         /// <param name="matrixB">Исходная матрица В (Вторая матрица)</param>
         private static void MatrixAddition(int[,] matrixA, int[,] matrixB)
         {
-            var matrix1 = matrixA;
-            var matrix2 = matrixB;
+            var matrixC = new int[GetMatrixRow(matrixA), GetMatrixCol(matrixA)];
 
             Console.Clear();
             Console.WriteLine("\nСложение матриц!");
-            SourceMatrices(matrix1, matrix2);
+            SourceMatrices(matrixA, matrixB);
 
-            if (GetMatrixRow(matrix1) == GetMatrixRow(matrix2) && GetMatrixCol(matrix1) == GetMatrixCol(matrix2))
+            if (GetMatrixRow(matrixA) == GetMatrixRow(matrixB) && GetMatrixCol(matrixA) == GetMatrixCol(matrixB))
             {
-                for (int i = 0; i < GetMatrixRow(matrix1); i++)
+                for (int i = 0; i < GetMatrixRow(matrixA); i++)
                 {
-                    for (int j = 0; j < GetMatrixCol(matrix1); j++)
+                    for (int j = 0; j < GetMatrixCol(matrixA); j++)
                     {
-                        matrix1[i, j] += matrix2[i, j];
+                        matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
                     }
                 }
 
                 Console.WriteLine("\nРезультат сложения матриц:");
-                PrintMatrix(matrix1);
+                PrintMatrix(matrixC);
             }
             else
             {
@@ -197,25 +198,24 @@ namespace HomeWork5._1
         /// <param name="matrixB">Исходная матрица В (Вторая матрица)</param>
         private static void MatrixDifference(int[,] matrixA, int[,] matrixB)
         {
-            var matrix1 = matrixA;
-            var matrix2 = matrixB;
+            var matrixC = new int[GetMatrixRow(matrixA), GetMatrixCol(matrixA)];
 
             Console.Clear();
             Console.WriteLine("\nВычитание матриц!");
-            SourceMatrices(matrix1, matrix2);
+            SourceMatrices(matrixA, matrixB);
 
-            if (GetMatrixRow(matrix1) == GetMatrixRow(matrix2) && GetMatrixCol(matrix1) == GetMatrixCol(matrix2))
+            if (GetMatrixRow(matrixA) == GetMatrixRow(matrixB) && GetMatrixCol(matrixA) == GetMatrixCol(matrixB))
             {
-                for (int i = 0; i < GetMatrixRow(matrix1); i++)
+                for (int i = 0; i < GetMatrixRow(matrixA); i++)
                 {
-                    for (int j = 0; j < GetMatrixCol(matrix1); j++)
+                    for (int j = 0; j < GetMatrixCol(matrixA); j++)
                     {
-                        matrix1[i, j] -= matrix2[i, j];
+                        matrixC[i, j] = matrixA[i, j] - matrixB[i, j];
                     }
                 }
 
                 Console.WriteLine("\nРезультат вычитания матриц:");
-                PrintMatrix(matrix1);
+                PrintMatrix(matrixC);
             }
             else
             {
@@ -225,18 +225,49 @@ namespace HomeWork5._1
             Console.ReadKey();
         }
 
-
+        /// <summary>
+        /// Умножение матриц
+        /// </summary>
+        /// <param name="matrixA">Исходная первая матрица (матрица которую умножают)</param>
+        /// <param name="matrixB">Исходная вторая матрица (матрица на которую умножают)</param>
         private static void MatrixMultiplication(int[,] matrixA, int[,] matrixB)
         {
-            var matrix1 = matrixA;
-            var matrix2 = matrixB;
+            var matrixC = new int[GetMatrixRow(matrixA), GetMatrixCol(matrixB)];
 
             Console.Clear();
             Console.WriteLine("\n\nПеремножение матриц!");
-            
-            SourceMatrices(matrix1, matrix2);
-            
+
+            SourceMatrices(matrixA, matrixB);
+
+            if (GetMatrixCol(matrixA) == GetMatrixRow(matrixB))
+            {
+                for (int i = 0; i < GetMatrixRow(matrixA); i++)
+                {
+                    for (int j = 0; j < GetMatrixCol(matrixB); j++)
+                    {
+                        matrixC[i, j] = 0;
+
+                        for (int k = 0; k < GetMatrixCol(matrixA); k++)
+                        {
+                            matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                        }
+                    }
+                }
+
+                Console.WriteLine("\n\nРезультат:");
+                PrintMatrix(matrixC);
+            }
+            else
+            {
+                Console.WriteLine("\n\n\nПеремножение матриц невозможно. Несоблюдены правила перемножения матриц " +
+                                  "(кол-во колнок матрицы А неравно кол-ву строк матрицы В).");
+            }
+            Console.ReadKey();
         }
+
+        #endregion
+
+
         static void Main(string[] args)
         {
             Random random = new Random();
@@ -267,6 +298,8 @@ namespace HomeWork5._1
             MatrixAddition(matrixA, matrixB);
             MatrixDifference(matrixA, matrixB);
             MatrixDifference(matrixB, matrixA);
+            MatrixMultiplication(matrixA, matrixB);
+            MatrixMultiplication(matrixB, matrixA);
 
             Console.ReadKey();
         }
