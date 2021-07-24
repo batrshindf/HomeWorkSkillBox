ae08a1f2bf429e5f458c9a1830727f72347bb0fa
Денис Батршин <batrshindf@yandex.ru>
2021-07-24 10:57:06 +0300

    Доработан метод нахождения слова/слов с максимальной длинной. Добавлены XML-комментарии к методам.

--- HomeWork5.2/Program.cs
+++ HomeWork5.2/Program.cs
@@ -8,11 +8,20 @@ namespace HomeWork5._2
 {
     class Program
     {
+        /// <summary>
+        /// Ввод строки пользователем
+        /// </summary>
+        /// <returns>Введённая строка</returns>
         private static string InputString()
         {
             Console.WriteLine("Введите строку с текстом:");
             return Console.ReadLine();
         }
+        /// <summary>
+        /// Разбивка строки на отдельные слова
+        /// </summary>
+        /// <param name="inputString">Строка, введённая пользователем</param>
+        /// <returns>Массив типа string заполненный отдельными словами</returns>
         private static string[] SeparatorStrings(string inputString)
         {
             char[] delimiterChars = { ' ', ',', '.', ':', ';', '(', ')', '\t', '\n' };
@@ -20,6 +29,10 @@ namespace HomeWork5._2
             string[] workingStrings = inputString.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
             return workingStrings;
         }
+        /// <summary>
+        /// Вывод слова или коллекции слов
+        /// </summary>
+        /// <param name="workingStrings">Массив строк после разбивки</param>
         private static void OutputWorkingStrings(string[] workingStrings)
         {
             foreach (var word in workingStrings)
@@ -27,6 +40,11 @@ namespace HomeWork5._2
                 Console.WriteLine($"\n{word}");
             }
         }
+        /// <summary>
+        /// Нахождение слова с минимальной длинной
+        /// </summary>
+        /// <param name="inputStrings">Строка, введённая пользователем</param>
+        /// <returns>Слово с минимальной длинной</returns>
         private static string WordMinLength(string inputStrings)
         {
             var workingStrings = SeparatorStrings(inputStrings);
@@ -44,11 +62,17 @@ namespace HomeWork5._2
 
             return workingStrings[position];
         }
+        /// <summary>
+        /// Находение слова/слов с максимвльной длинной
+        /// </summary>
+        /// <param name="inputString">Строка, введённая пользователем</param>
+        /// <returns>Слово/коллекция слов</returns>
         private static string[] WordMaxLength(string inputString)
         {
             var workingStrings = SeparatorStrings(inputString);
             var max = workingStrings[0].Length;
             string[] returnWords = new string[workingStrings.Length];
+            var countReturnWords = 0;
 
             for (int i = 1; i < workingStrings.Length; i++)
             {
@@ -58,14 +82,24 @@ namespace HomeWork5._2
                 }
             }
 
-            return max;
+            for (int i = 0; i < workingStrings.Length; i++)
+            {
+                if (max == workingStrings[i].Length)
+                {
+                    returnWords[countReturnWords] = workingStrings[i];
+                    countReturnWords++;
+                }
+            }
+
+            return returnWords;
         }
         static void Main(string[] args)
         {
             var inputString = InputString();
 
-            Console.WriteLine($" Слово с минимальной длинной:  {WordMinLength(inputString)}");
-
+            Console.WriteLine($"\nСлово с минимальной длинной:\n\n{WordMinLength(inputString)}");
+            Console.WriteLine($"\nСлово/слова с максимальной длинной:");
+            OutputWorkingStrings(WordMaxLength(inputString));
 
             Console.ReadKey();
         }
